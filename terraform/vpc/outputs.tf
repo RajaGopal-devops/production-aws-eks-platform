85fb2d9d4c9cc882dee5bb6739186e3187a668d8
RajaGopal <rajagopal.bkvl@gmail.com>
2026-08-29 18:34:22 +0530

    feat(vpc): add EKS VPC terraform configuration and gitignore

---
+++ terraform/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
+}
+
+output "public_subnet_ids" {
+  value       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  description = "IDs of the public subnets"
+}
